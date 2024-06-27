cbaceeae342e2b9eddb99acf0a2ea7bda681a7a7
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-27 15:56:17 -0600

    chore: Update npm dependencies and add Tailwind CSS library

--- components/Guest.tsx
+++ components/Guest.tsx
@@ -1,11 +1,20 @@
 import { SignInButton } from "@clerk/nextjs";
+import Image from 'next/image';
 
 const Guest = () => {
   return (
     <div className="guest">
     <h1>Welcome</h1>
     <p>Please sign in to mange your transactions</p>
-    <SignInButton />
+      <SignInButton />
+      <p className="description">Screen Shot of Dashboard</p>
+      <div className="image">
+        <Image
+          src="/images/img-01.png"
+          width={400}
+          height={400}
+          alt="Picture of Exspense Tracker" />
+        </div>
     </div>
   );
 }
